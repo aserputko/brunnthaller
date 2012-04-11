a683d491c918ad1e130a533ea16fc49e8ef26b62
Andrew Serputko <aserputko@gmail.com>
2012-04-12 00:40:45 +0400

    Created new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brunnthaller::Application.config.secret_token = '1e289c11f5d95096be40d2dc486734a6c908eb0365c62d12c1e8765e9cf19c38bbbf8a733cb1661f71a3d5c4e9be824ef14eb8392ce10812d3ae1e17be20b6ef'
